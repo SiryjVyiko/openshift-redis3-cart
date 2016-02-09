f37aec57372227c585867293d6773c7e022a7c2b
SiryjVyiko <dz@hivext.net>
2016-02-09 12:18:42 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -2,23 +2,12 @@
 
 SED=$(which sed);
 
-#
-# This is an example of reset password hook in Jelastic
-#
-
-#$J_OPENSHIFT_APP_ADM_USER        ;   Operate this variable for the username
-#$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
-
 function _setPassword() {
+	chmod 755 /opt/repo/env
         service cartridge stop > /dev/null 2>&1;
         echo $J_OPENSHIFT_APP_ADM_PASSWORD > ${CARTRIDGE_HOME}/env/REDIS_PASSWORD;
         export REDIS_PASSWORD=$J_OPENSHIFT_APP_ADM_PASSWORD;
         saveEnvironmentVariables;
         service cartridge start > /dev/null 2>&1;
+	chmod 555 /opt/repo/env
 }
-
-
-
-
-
-
